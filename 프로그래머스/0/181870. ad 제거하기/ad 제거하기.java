78a492a0e72b6063e40d36977b07c5d93337541d
jiwonKang <145153564+wonji426@users.noreply.github.com>
2024-10-23 23:05:00 +0900

    [level 0] Title: ad 제거하기, Time: 1.05 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public List solution(String[] strArr) {
+        List<String> answer = new ArrayList<>();
+        for(int i = 0; i<strArr.length; i++){
+            if(!strArr[i].contains("ad")){
+                answer.add(strArr[i]);
+            }
+        }
+        return answer;
+    }
+}
