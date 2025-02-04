eade7daa781dafd51c16def8669414096d53fd3e
jiwonKang <145153564+wonji426@users.noreply.github.com>
2025-02-04 15:28:57 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.53 ms, Memory: 86 MB -BaekjoonHub

---
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] sides) {
+        int answer = 0;
+        Arrays.sort(sides);
+        int min = sides[0];
+        int max = sides[1];
+
+        for(int i = max - min + 1 ; i <= max ; i++){
+            answer++;
+        }
+        
+        for(int i = max + 1 ; i < min + max ; i++){
+            answer++;
+        }
+        return answer;
+    }
+}
